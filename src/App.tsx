c2b79c4751ba1c93c1719605cc0a505ec6152829
Mohamed Adel <ma7598101@gmail.com>
2025-05-15 20:43:51 +0300

    SETUP: file for each page and react-route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import './App.css'
+import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import HomePage from "@/pages/home";
+import AboutPage from "@/pages/about";
+import LoginPage from "@/pages/login";
+import ProfilePage from "@/pages/profile";
+import EditProfilePage from "@/pages/edit-profile";
 
 function App() {
-
   return (
-    <div>
-      hello there
-    </div>
-  )
+    <ThemeProvider defaultTheme="system" storageKey="ui-theme">
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/edit-profile" element={<EditProfilePage />} />
+        </Routes>
+      </Router>
+      <Toaster position="bottom-right" />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/edit-profile.tsx
@@ -0,0 +1,3 @@
+export default function EditProfilPage() {
+  return <div>hello there</div>;
+}
